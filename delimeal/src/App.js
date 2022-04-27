a2f1554ce83885e6f9ec6249e7ac86aaaa847e99
BetrayerMagik <104353299+BetrayerMagik@users.noreply.github.com>
2022-04-27 19:08:54 -0400

    frontend

---
+++ delimeal/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="relative w-screen h-auto">
+      homepage
+    </div>
+  );
+}
+
+export default App;
